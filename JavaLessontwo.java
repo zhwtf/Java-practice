c15e28edcd891984cd5ea9305d9685bff6526a32
haozheng <zhwtf123@gmail.com>
2017-02-03 15:50:33 -0800

    02-03-02

--- JavaLessontwo.java
+++ JavaLessontwo.java
@@ -14,6 +14,22 @@ public class JavaLessontwo
 
         System.out.println("You entered " + numberEntered);
 
+        int numEnteredTimes2 = numberEntered + numberEntered;
+        System.out.println(numberEntered + " + " + numberEntered + " = " + numEnteredTimes2);
+
+        int numberABS = Math.abs(numberEntered);
+        int whichIsBigger = Math.max(5, 7);
+        int whichIsSmaller = Math.max(5, 8);
+
+        double numSqrt = Math.sqrt(5.23);
+
+        int numCeiling = (int) Math.ceil(5.23); //6
+
+        int randomNumber = (int) (Math.random() * 11); //random 0 to 10 inclusive
+        System.out.println("randomNumber is " + randomNumber);
+        
+
+
     }
     else{
       System.out.println("Enter an integer next time");
