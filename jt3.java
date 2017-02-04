2ff22df163a9ae0a8301a27a422be4039e5ccd25
haozheng <zhwtf123@gmail.com>
2017-02-03 16:29:34 -0800

    02-03-03

---
+++ jt3.java
@@ -0,0 +1,15 @@
+public class jt3{
+    public static void main(String[] args){
+        int randomNumber = (int) (Math.random() * 50);
+
+        if (randomNumber < 25){
+          System.out.println("the random number is less than 25");
+
+        }
+        else if (randomNumber > 40){
+          System.out.println("the random number is greater than 40");
+        }
+
+        
+    }
+}
