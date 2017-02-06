b380a1158965a41f67f0dfeedeabe55b68dfd452
haozheng <zhwtf123@gmail.com>
2017-02-05 16:56:28 -0800

    02-05-01

---
+++ Head First-java/ThreadTestDrive.java
@@ -0,0 +1,11 @@
+
+
+public class ThreadTestDrive{
+    public static void main(String[] args){
+        Runnable threadJob = new MyRunnable();
+        Thread myThread = new Thread(threadJob);
+
+        myThread.start();
+        System.out.println("back to main");
+    }
+}
