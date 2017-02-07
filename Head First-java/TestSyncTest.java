4b259fe3aad407df015cfbf85943896e5cce2baf
haozheng <zhwtf123@gmail.com>
2017-02-06 22:35:20 -0800

    02-06-02

---
+++ Head First-java/TestSyncTest.java
@@ -0,0 +1,25 @@
+class TestSync implements Runnable{
+    private int balance;
+
+    public void run(){
+        for(int i = 0; i < 50; i++){
+            increment();
+            System.out.println("balance is " + balance);
+        }
+    }
+
+    public void increment(){
+        int i = balance;
+        balance = i + 1;
+    }
+}
+
+public class TestSyncTest{
+    public static void main(String[] args){
+        TestSync job = new TestSync();
+        Thread a = new Thread(job);
+        Thread b = new Thread(job);
+        a.start();
+        b.start();
+    }
+}
